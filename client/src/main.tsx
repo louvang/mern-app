23696672634d73f946bc06b8f794bfa4dd7bdbf5
Lou Vang <louvangdev@gmail.com>
2024-06-21 20:50:53 -0500

    Add styling and routes to client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import App from './App.tsx';
-import './assets/styles/main.css';
+import Register from './pages/Register.tsx';
+import Login from './pages/Login.tsx';
+import './assets/styles/main.scss';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
